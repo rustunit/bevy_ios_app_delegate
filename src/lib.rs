d8f49e78ddfa9300a5b79787ab9987cfdc9d66ad
extrawurst <mail@rusticorn.com>
2025-05-16 11:51:12 +0200

    more docs

--- src/lib.rs
+++ src/lib.rs
@@ -7,11 +7,17 @@ mod channel;
 #[cfg(any(target_os = "ios"))]
 mod swizzle;
 
+/// Event being triggered based on AppDelegate calls
 #[derive(Event, Clone, Debug)]
 pub enum AppDelegateCall {
+    /// triggered after app being opened or foregrounded based on a click on a URL schema
+    /// see <https://developer.apple.com/documentation/xcode/defining-a-custom-url-scheme-for-your-app>
     OpenURL(String),
 }
 
+/// Plugin to hook into iOS app delegate calls
+/// It will use swizzling to hook into UIApplication init to register our custom AppDelegate as soon as winit starts the UIKit initialization procedure
+/// This only takes effect on ios builds
 pub struct IosAppDelegatePlugin;
 
 impl Plugin for IosAppDelegatePlugin {
